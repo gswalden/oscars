745f4b16016d5a730fd937d20e9e0b1e8ee32766
Greg Walden <talk@gsw.nyc>
2017-02-26 16:00:43 -0500

    v0.0.0

---
+++ test/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const chai = require('chai')
+  , should = chai.should()
+  , app = require('../')
+  ;
+
+describe('module tests', () => {
+  it('should pass', done => {
+    done();
+  });
+})
